89f2cda61794e9ac47f8491173bbb6c8f5b994ad
metasebiya <metibizu@gmail.com>
2022-03-12 23:21:01 +0300

    102-print_comb5

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+/**
+ * main - prints all possible combination of two digit numbers.
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int i;
+	int j;
+
+	for (i = 0; i <= 9; i++)
+	{
+		for (j = 0; j <= 9; j++)
+		{
+			putchar(i + '0');
+			putchar(j + '0');
+			if (i != '9' || j != '9')
+			{
+				putchar(',');
+				putchar(' ');
+			}
+		}
+	}
+
+	putchar('\n');
+
+	return (0);
+
+}
