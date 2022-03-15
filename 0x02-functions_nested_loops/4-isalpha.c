4f0ac290f73f6d28e85cc5bf64b0bd9d4242094d
metasebiya <metibizu@gmail.com>
2022-03-15 22:45:13 +0300

    4-isalpha

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * _isalpha - checks whether the passed character is an alphabet (A-Z or a-z)
+ * @c: The character to check
+ *
+ * Return: 1 if c is an alphabet, otherwise Return: 0
+ */
+int _isalpha(int c)
+{
+	int i;
+	i = c;
+
+	if(i >= 97 && i <= 122)
+		return (1);
+	else if (i >= 65 && i <= 90)
+		return (1);
+	else
+		return (0);
+}
+
